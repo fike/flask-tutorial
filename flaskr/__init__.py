611b9698a4553bf4e268c3c058410518302a1aa8
Fernando Ike <fernando.ike@gmail.com>
2020-12-26 10:55:23 -0300

    remove unused code

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -60,9 +60,3 @@ def init_db():
     db.create_all()
 
  
-@click.command("init-db")
-@with_appcontext
-def init_db_command():
-    """Clear existing data and create new tables."""
-    init_db()
-    click.echo("Initialized the database.")
